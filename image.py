298c2ea36a61d279f7eb5f486ab9b7eb47096c7b
Billy <dinner0000@gmail.com>
2018-06-26 15:12:57 -0700

    simulation

--- image.py
+++ image.py
@@ -4,6 +4,8 @@ import myFunc as mf
 import cv2
 
 while True:
+	
+	
 	# Run when Arduino signals a new strip
 	# x and y position accumulators
 	# acc_x = 0
@@ -11,9 +13,24 @@ while True:
 
 	# number of pixels accumulated
 	# acc_count = 0
+	CAPTURES_PER_STRIP = 5
 
-	image = mf.isolateColors('tape2.jpg', 300, 300, 10)
+	image = cv2.imread('tape.png')
+	image = cv2.resize(image, (100, 100))
+	image = mf.isolateColors(image, 10)
+	tempStripHeight = 0
+	stripHeights = []
+	stripCount = 0
+	numCaptures = 0
+	
+	#This will be sent to Arduino
+	fullArray = np.zeros((image.shape[1] * CAPTURES_PER_STRIP, image.shape[0]))
 
+	while numCaptures < CAPTURES_PER_STRIP:		
+		# image = cv2.imread('tape.png')
+		# image = cv2.resize(image, (100,100))
+		# height, width, channels = image.shape
+		# test = np.zeros((height * CAPTURES_PER_STRIP, width))
 
 	# check the accumulator is greater than 0, to avoid a divide by 0
 	# if acc_count > 0:
@@ -21,8 +38,14 @@ while True:
 		# mean_x = acc_x / acc_count
 		# mean_y = acc_y / acc_count
 
-	gray_image = mf.convertToGrayScale(image)
-	mArray = mf.mapToBinary(gray_image)
+		gray_image = mf.convertToGrayScale(image)
+		mArray = mf.mapToBinary(gray_image)
+		tempStripHeight = mf.detectStripHeight(mArray)
+		if tempStripHeight != -1 :
+			stripHeights.append(tempStripHeight + numCaptures*image.shape[1])
+			stripCount += 1
+		fullArray[image.shape[1]*numCaptures:image.shape[1]*(numCaptures + 1) - 1, 0:image.shape[0]-1] = mArray[0:image.shape[1] - 1, 0:image.shape[0] - 1]
+		
 		# # draw a small cross in red at the mean position
 		# image[mean_x + 0, mean_y - 1] = 255, 0, 0
 		# image[mean_x - 1, mean_y + 0] = 255, 0, 0
@@ -31,6 +54,8 @@ while True:
 		# image[mean_x + 0, mean_y + 1] = 255, 0, 0
 	# display the image on the viewer
 
-	print (mArray)
-	cv2.imshow("images", mArray)
+		numCaptures += 1
+
+	#fullArray = mf.fillBetweenStrips(fullArray,stripCount,stripHeights)	
+	cv2.imshow("images", fullArray)
 	cv2.waitKey(0)
--- myFunc.py
+++ myFunc.py
@@ -2,15 +2,16 @@ import numpy as np
 import cv2
 
 
-def isolateColors(imageName, resizeX, resizeY, thresh):
-    # grab an image from the camera
-    image = cv2.imread(imageName)
-    image = cv2.resize(image, (resizeX,resizeY))
+def isolateColors(image, thresh):
+    # grab an image from the camera        
+
+    
     height, width, channels = image.shape
 	# iterate over every pixel
-    for x in range(0, width-1):
-           for y in range(0, height-1):
-			# get the value of the current pixel
+    
+    for x in range(0 , width-1):
+        for y in range(0, height-1):
+	    #get the value of the current pixel
             red, green, blue = image[y, x]
 
 			# check if the red intensity is greater than green and blue
@@ -24,10 +25,11 @@ def isolateColors(imageName, resizeX, resizeY, thresh):
                 image[y, x] = 255, 255, 255
             else :
                 image[y, x] = 0, 0, 0
+
+
     return image
 
-def mapToBinary(anArray) :
-
+def mapToBinary(anArray):
     return np.ceil(anArray/255)    
    
 
@@ -35,9 +37,23 @@ def convertToGrayScale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 def detectStripHeight(anArray):
-    print('TO DO')
+    for x in range(anArray.shape[1]-1):
+        for y in range(anArray.shape[0]-1):
+            if anArray[x, y] == 1 and anArray[x + 1, y + 1] == 1:
+                return x
+    return -1
+             
+
+
+    #Will take processed array as param of the whole strip an return an array of strip heights'
     
 
+
+#anArray: fully processed array matching the height of the full cycle
+#stripCount: How many tape strips are in the cycle
+#stripHeights: A list of strip positions
+
+#This function works based on the assumption that it will encounter only a single strip in a full cycle (covering a wall plug) OR contains only pairs of strips
 def fillBetweenStrips(anArray, stripCount, stripHeights):
 
     width = anArray.shape[1]
